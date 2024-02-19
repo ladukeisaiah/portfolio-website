4b75c7926e76cc13ad2a80b5ca2438561b21820e
Isaiah LaDuke <iladuke@atsapllc.com>
2024-02-19 18:08:31 -0500

    feat: added to project section and email section

--- src/app/Components/ProjectsSection.jsx
+++ src/app/Components/ProjectsSection.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import ProjectCard from './ProjectCard';
+import ProjectTag from './ProjectTag';
 
 const projectsData = [
     {
@@ -23,10 +25,53 @@ const projectsData = [
 ]
 
 const ProjectsSection = () => {
+    const [tag, setTag] = useState("All");
+
+    const handleTagChange = (newTag) => {
+        setTag(newTag);
+    };
+
+    const filteredProjects = projectsData.filter((project) =>
+        project.tag.includes(tag)
+    );
+
   return (
     <>
-    <div>ProjectsSection</div>
-    <div>{projectsData.map((project) => <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image}  />)}</div>
+    <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
+        My Projects
+    </h2>
+    <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
+        <ProjectTag 
+        onClick={handleTagChange} 
+        name="All" 
+        isSelected={tag === "All"}
+        />
+        <ProjectTag 
+        onClick={handleTagChange} 
+        name="Web" 
+        isSelected={tag === "Web"}
+        />
+        <ProjectTag 
+        onClick={handleTagChange} 
+        name="Mobile" 
+        isSelected={tag === "Mobile"}
+        />
+        <ProjectTag 
+        onClick={handleTagChange} 
+        name="UX/UI Prototypes" 
+        isSelected={tag === "UX/UI Prototypes"}
+        />
+    </div>
+    <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
+        {filteredProjects.map((project) => (
+        <ProjectCard key={project.id} 
+            title={project.title} 
+            description={project.description} 
+            imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}  />
+        ))}
+    </div>
     </>
   );
 };

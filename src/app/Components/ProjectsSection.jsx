4d64a03cb3f06ca8f40e229f8495606f9c12af81
Isaiah LaDuke <iladuke@atsapllc.com>
2024-02-20 18:38:46 -0500

    feat: animation additions

--- src/app/Components/ProjectsSection.jsx
+++ src/app/Components/ProjectsSection.jsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ProjectCard from './ProjectCard';
 import ProjectTag from './ProjectTag';
+import { motion, useInView } from "framer-motion";
 
 const projectsData = [
     {
@@ -26,6 +27,8 @@ const projectsData = [
 
 const ProjectsSection = () => {
     const [tag, setTag] = useState("All");
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
 
     const handleTagChange = (newTag) => {
         setTag(newTag);
@@ -35,8 +38,13 @@ const ProjectsSection = () => {
         project.tag.includes(tag)
     );
 
+    const cardVariants = {
+        initial: { y: 50, opacity: 0},
+        animate: { y: 0, opacity: 1},
+    }
+
   return (
-    <>
+    <section>
     <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
         My Projects
     </h2>
@@ -62,17 +70,24 @@ const ProjectsSection = () => {
         isSelected={tag === "UX/UI Prototypes"}
         />
     </div>
-    <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-        {filteredProjects.map((project) => (
+    <ul ref={ref} className='grid md:grid-cols-3 gap-8 md:gap-12'>
+        {filteredProjects.map((project, index) => (
+        <motion.li key={index} 
+        variants={cardVariants} 
+        initial="initial" 
+        animate={isInView ? "animate" : "initial"}
+        transition={{duration: 0.3, delay: index * 0.4}}
+        >
         <ProjectCard key={project.id} 
             title={project.title} 
             description={project.description} 
             imgUrl={project.image}
             gitUrl={project.gitUrl}
             previewUrl={project.previewUrl}  />
+        </motion.li>
         ))}
-    </div>
-    </>
+    </ul>
+    </section>
   );
 };
 

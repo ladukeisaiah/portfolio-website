d9e713d19d2e1087291d4b642a789ae997e67235
Isaiah LaDuke <iladuke@atsapllc.com>
2024-02-21 14:44:55 -0500

    fix: ESLint changes that need to be made to "'" in order for the build to work properly

--- src/app/Components/EmailSection.jsx
+++ src/app/Components/EmailSection.jsx
@@ -43,11 +43,11 @@ const EmailSection = () => {
         > 
         <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
         <div className='z-10'>
-            <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
+            <h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'>
             {" "}
-            I'm currently looking for new opportunities and would love to connect.
-            Whether you have a question or just want to say hi, I'll try my best 
+            I&apos;m currently looking for new opportunities and would love to connect.
+            Whether you have a question or just want to say hi, I will try my best 
             to get back to you!
             </p>
             <div className='socials flex flex-row gap-2'>
@@ -63,12 +63,12 @@ const EmailSection = () => {
             <form className='flex flex-col' onSubmit={handleSubmit}>
                 <div className='mb-6'>
                 <label 
-                htmlFor="email" 
+                htmlFor="email"
                 className='text-white block text-sm mb-2 font-medium'>
                     Your Email
                 </label>
                 <input 
-                name='email'
+                name="email"
                 type="email" 
                 id="email" 
                 className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'

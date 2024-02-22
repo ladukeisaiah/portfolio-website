1addbac98a6fb68a7f0fdb32f97c4890236ee273
Isaiah LaDuke <iladuke@atsapllc.com>
2024-02-22 17:49:52 -0500

    feat: adding more projects to the portfolio website

--- src/app/Components/ProjectsSection.jsx
+++ src/app/Components/ProjectsSection.jsx
@@ -16,6 +16,15 @@ const projectsData = [
     },
     {
         id: 2,
+        title: "Registration Form",
+        description: "A project creating registration forms using HTML & CSS",
+        image: "/images/projects/registration-form.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/ladukeisaiah/registration-form",
+        previewUrl: "/"
+    },
+    {
+        id: 3,
         title: "Blog Post Website",
         description: "A blog website built with HTML, CSS, & JS, for sharing posts on forestry and allowing users to post their own blogs to the site as well.",
         image: "",
@@ -23,6 +32,78 @@ const projectsData = [
         gitUrl: "ladukeisaiah.github.io/trial-by-fire/",
         previewUrl: "/"
     },
+    {
+        id: 4,
+        title: "SVG World Map",
+        description: "An SVG world map using REST APIs in order to gather data from worldbank.org and displaying them as countries are selected",
+        image: "/images/projects/svg-world-map.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/ladukeisaiah/SVG-World-Map",
+        previewUrl: "/"
+    },
+    {
+        id: 5,
+        title: "Quiz",
+        description: "This project is a quiz application built with HTML and CSS",
+        image: "/images/projects/quiz.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/ladukeisaiah/accessibility-quiz",
+        previewUrl: "/"
+    },
+    {
+        id: 6,
+        title: "Cafe Menu",
+        description: "This is a cafe menu built using HTML & CSS",
+        image: "/images/projects/cafe-menu.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/ladukeisaiah/cafe-menu",
+        previewUrl: "/"
+    },
+    {
+        id: 7,
+        title: "Cat Photo App",
+        description: "A web app displaying HTML & CSS",
+        image: "/images/projects/cat-photo-app.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/ladukeisaiah/cat-photo-app",
+        previewUrl: "/"
+    },
+    {
+        id: 8,
+        title: "Rothko Painting",
+        description: "A project for practicing CSS design styles and their capabilities",
+        image: "/images/projects/rothko-painting.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/ladukeisaiah/Rothko-painting",
+        previewUrl: "/"
+    },
+    {
+        id: 9,
+        title: "Nutrition Label",
+        description: "This project is for practicing CSS design principals by building out a nutrition label",
+        image: "/images/projects/nutrition-label.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/ladukeisaiah/nutrition-label",
+        previewUrl: "/"
+    },
+    {
+        id: 10,
+        title: "Flexbox Photo Gallery",
+        description: "A project for practing CSS flex-box designs principles",
+        image: "/images/projects/Flexbox-photo-gallery.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/ladukeisaiah/flexbox-photo-gallery",
+        previewUrl: "/"
+    },
+    {
+        id: 11,
+        title: "Color Markers",
+        description: "A project for practing CSS color designs and principles",
+        image: "/images/projects/color-markers.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/ladukeisaiah/flexbox-photo-gallery",
+        previewUrl: "/"
+    }
 ]
 
 const ProjectsSection = () => {

8dd59eb04f08797c4b1cb00eb0430d3bfb06f59a
Isaiah LaDuke <iladuke@atsapllc.com>
2024-03-19 19:40:26 -0400

    fix: this is an attempt to fix the height issue thats present with the animation

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -28,7 +28,7 @@ const HeroSection = () => {
     return (
         
         <section className="lg:py-16">
-            <div className="grid grid-cols-1 lg:grid-cols-12">
+            <div className="grid grid-cols-1 lg:grid-cols-12 min-h-[500px]">
                 <motion.div 
                 initial={{opacity: 0, scale: 0.5}} 
                 animate={{opacity: 1, scale: 1}} 
